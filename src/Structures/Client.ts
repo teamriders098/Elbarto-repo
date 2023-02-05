946239f0a9a693a0162eba10ac2b865d503892e0
Empty-Sama <116464251+Empty-sama@users.noreply.github.com>
2023-02-05 21:06:53 +0200

    Update Client.ts

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -23,7 +23,7 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
             chatBotUrl: process.env.CHAT_BOT_URL || '',
             mods: (process.env.MODS || '').split(', ').map((user) => `${user}@s.whatsapp.net`),
             PORT: Number(process.env.PORT || 3000),
-            casinoGroup: ''
+            casinoGroup: '120363037700352694@g.us'
         }
         new Server(this)
     }
