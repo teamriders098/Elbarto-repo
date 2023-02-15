d06b60ff6856728e788e8980475401b67cca554b
Elbarto <113073680+elbarto2m@users.noreply.github.com>
2023-02-15 22:05:38 +0100

    Update Help.ts

--- src/Commands/General/Help.ts
+++ src/Commands/General/Help.ts
@@ -18,13 +18,9 @@ export default class extends BaseCommand {
             })).filter((command) => command.data.config.category !== 'dev')
             const { nsfw } = await this.client.DB.getGroup(M.from)
             if (!nsfw) commands = commands.filter(({ data }) => data.config.category !== 'nsfw')
-            const buffer = this.client.assets.get('chisato') as Buffer
-            let text = `\n*╭─「 (づ￣ ³￣)づ 」*
-*│ ɴᴀᴍᴇ:* ${this.client.config.name}
-*│ ᴜsᴇʀ: @${M.sender.jid.split('@')[0]}*
-*│ ᴘʀᴇғɪx:* "${this.client.config.prefix}"
-*│ ᴏᴡɴᴇʀ:* *Dreaded.inc*
-*╰────────────┈平和* \n\nThese are my available commands ツ`
+            let text = `👋🏻 (💙ω💙) Konichiwa! *@${M.sender.jid.split('@')[0]}*, I'm ${
+                this.client.config.name
+            }\nMy prefix is - "${this.client.config.prefix}"\n\nThe usable commands are listed below.`
             const categories: string[] = []
             for (const command of commands) {
                 if (categories.includes(command.data.config.category)) continue
@@ -33,27 +29,27 @@ export default class extends BaseCommand {
             for (const category of categories) {
                 const categoryCommands: string[] = []
                 const filteredCommands = commands.filter((command) => command.data.config.category === category)
-                text += `\n\n*${this.client.utils.capitalize(category)}*\n`
+                text += `\n\n*━━━❰ ${this.client.utils.capitalize(category)} ❱━━━*\n\n`
                 filteredCommands.forEach((command) => categoryCommands.push(command.data.name))
-                text += `❐ \`\`\`${categoryCommands.join(', ')}\`\`\``
+                text += `\`\`\`${categoryCommands.join(', ')}\`\`\``
             }
-            text += `\n\n⚠️ *Note:* Use ${this.client.config.prefix}help <command_name> for more info of a specific command. Example: *${this.client.config.prefix}help hello*`
-            return void (await M.reply(buffer, 'image', undefined, undefined, text, [M.sender.jid]))
+            text += `\n\n📕 *Note:* Use ${this.client.config.prefix}help <command_name> for more info of a specific command. Example: *${this.client.config.prefix}help hello*`
+            return void (await M.reply(text, 'text', undefined, undefined, undefined, [M.sender.jid]))
         } else {
             const cmd = context.trim().toLowerCase()
             const command = this.handler.commands.get(cmd) || this.handler.aliases.get(cmd)
             if (!command) return void M.reply(`No command found | *"${context.trim()}"*`)
             return void M.reply(
-                `❗ *Command:* ${this.client.utils.capitalize(command.name)}\n🎱 *Aliases:* ${
+                `🎐 *Command:* ${this.client.utils.capitalize(command.name)}\n🎴 *Aliases:* ${
                     !command.config.aliases
                         ? ''
                         : command.config.aliases.map((alias) => this.client.utils.capitalize(alias)).join(', ')
-                }\n🧩 *Category:* ${this.client.utils.capitalize(command.config.category)}\n⏰ *Cooldown:* ${
+                }\n🔗 *Category:* ${this.client.utils.capitalize(command.config.category)}\n⏰ *Cooldown:* ${
                     command.config.cooldown ?? 3
-                }s\n🛠️ *Usage:* ${command.config.usage
+                }s\n🎗 *Usage:* ${command.config.usage
                     .split('||')
                     .map((usage) => `${this.client.config.prefix}${usage.trim()}`)
-                    .join(' | ')}\n📚 *Description:* ${command.config.description}`
+                    .join(' | ')}\n🧧 *Description:* ${command.config.description}`
             )
         }
     }
