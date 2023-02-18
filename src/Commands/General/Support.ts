d3fc7201e743ca1ddaba99041a74c425bfd89ce2
Empty-Sama <116464251+Empty-sama@users.noreply.github.com>
2023-02-19 01:57:47 +0200

    Create Support.ts

---
+++ src/Commands/General/Support.ts
@@ -0,0 +1,28 @@
+import { BaseCommand, Command, Message } from '../../Structures'
+import { AnyMessageContent } from '@adiwajshing/baileys'
+
+@Command('support', {
+    description: '',
+    category: 'general',
+    usage: 'support',
+    cooldown: 15,
+    exp: 10,
+    dm: true
+})
+export default class command extends BaseCommand {
+    override execute = async (M: Message): Promise<void> => {
+        let url!: string
+        let text = 'dreaded'
+        const { supportGroups } = this.client.config
+        for (let i = 0; i < supportGroups.length; i++) {
+            const { subject } = await this.client.groupMetadata(supportGroups[i])
+            const code = await this.client.groupInviteCode(supportGroups[i])
+            text += `\n*#${i + 1}*\n*${subject}:* *https://chat.whatsapp.com/${code}*\n`
+            if (!url) url = `https://chat.whatsapp.com/${code}`
+        }
+        await this.client.sendMessage(M.sender.jid, {
+            text
+        } as unknown as AnyMessageContent)
+        return void M.reply('Sent the group links in your DM🦈')
+    }
+}
