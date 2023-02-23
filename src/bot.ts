bffe7e57d1200e95a16dde80c1a23ba5fdc28dd1
Elbarto <113073680+elbarto2m@users.noreply.github.com>
2023-02-23 01:24:27 +0100

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,46 +1,25 @@
 import { Client } from './Structures'
-import { MessageHandler, AssetHandler, CallHandler, EventHandler, NewsHandler, ModHandler } from './Handlers'
-;(async () => {
+import { MessageHandler, AssetHandler, CallHandler, EventHandler } from './Handlers'
+;(async (): Promise<void> => {
     const client = new Client()
 
     await client.start()
 
     new AssetHandler(client).loadAssets()
 
-    const messageHandler = new MessageHandler(client)
+    const { handleMessage, loadCommands } = new MessageHandler(client)
 
     const { handleEvents, sendMessageOnJoiningGroup } = new EventHandler(client)
 
     const { handleCall } = new CallHandler(client)
 
-    const modHandler = new ModHandler(client)
+    loadCommands()
 
-    const newsHandler = new NewsHandler(client, messageHandler)
-
-    messageHandler.loadCommands()
-
-    client.on('new_message', async (M) => await messageHandler.handleMessage(M))
+    client.on('new_message', async (M) => await handleMessage(M))
 
     client.on('participants_update', async (event) => await handleEvents(event))
 
-    client.on('new_group_joined', async (group) => {
-        messageHandler.groups.push(group.jid)
-        await sendMessageOnJoiningGroup(group)
-    })
+    client.on('new_group_joined', async (group) => await sendMessageOnJoiningGroup(group))
 
     client.on('new_call', async (call) => await handleCall(call))
-
-    client.on(
-        'pokemon_levelled_up',
-        async (data) =>
-            await messageHandler.handlePokemonStats(data.M, data.pokemon, data.inBattle, data.player, data.user)
-    )
-
-    client.once('open', async () => {
-        messageHandler.groups = await client.getAllGroups()
-        await messageHandler.loadCardsEnabledGroups()
-        await messageHandler.loadWildEnabledGroups()
-        await newsHandler.loadNewsEnabledGroups()
-        await modHandler.loadMods()
-    })
 })()
